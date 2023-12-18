3aee9ba061b83cdf3a4263fe65fe719124962a15
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2023-12-19 02:21:19 +0300

    Delete models directory

--- models/brand.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var bcrypt = require('bcryptjs');
-var BrandSchema = new mongoose.Schema({
-    brandNo: Number,
-    name: String
-});
-
-var Brand = mongoose.model('brand', BrandSchema);
-module.exports = Brand;
